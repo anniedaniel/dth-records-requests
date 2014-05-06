52de9422ae9dc5d1903a1215214457649bc431f9
anniedaniel <anniedaniel001@gmail.com>
2014-05-05 23:32:08 -0400

    chart displaying the fequency of tags

--- js/success.js
+++ js/success.js
@@ -32,37 +32,104 @@ $(function () {
 });
 
 $(function () {
-    $('#year').highcharts({
+    $('#tags').highcharts({
         chart: {
-            type: 'bar'
+            type: 'column'
         },
         title: {
-            text: 'When?'
-        },
-        xAxis: {
-            categories: ['2009', '2010', '2011', '2012', '2013', '2014']
+            text: 'Requests'
         },
         yAxis: {
             title: {
-                text: 'Requests'
+                text: 'Tags'
             }
         },
         series: [{
-            name: 'Yes',
-            data: [4, 29]
+            name: 'Email', 
+            data: [25]
         }, {
-            name: 'Denied',
-            data: [0, 3]
+            name: 'Students',
+            data: [10]
         }, {
-          name: 'Partially',
-          data: [0, 1]
+          name: 'Sports',
+          data: [66]
         }, {
-          name: 'Outstanding',
-          data: [0, 4]
+          name: 'Football',
+          data: [31]
+        }, {
+          name: 'Basketball',
+          data: [9]
+        }, {
+          name: 'Money',
+          data: [56]
+        }, {
+          name: 'Contracts',
+          data: [27]
+        }, {
+          name: 'Budgets',
+          data: [9]
+        }, {
+          name: 'Phone Calls',
+          data: [11]
+        }, {
+          name: 'Courses',
+          data: [13]
+        }, {
+          name: 'Sexual Assault',
+          data: [12]
+        }, {
+          name: 'Crime',
+          data: [17]
+        }, {
+          name: 'Arts',
+          data: [4]
         }],
-
     });
 });
+
+$(function () {
+  // Build the chart
+    $('#reporters').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Request Topics'
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Tags',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                },
+                ['Safari',    8.5],
+                ['Opera',     6.2],
+                ['Others',   0.7]
+            ]
+        }]
+    });
+  });
 // $.get('js/records.xml', function(xml) {
 //   var $xml = $(xml);
 //   $xml.find('success').each(function(i, success){
